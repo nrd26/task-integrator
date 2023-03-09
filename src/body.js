faaef593a1dea922f5cd15ae7f6dc84036ca362d
Nihal Rian Dias <nihaldias261@gmail.com>
2023-03-09 23:51:52 +0530

    CRUD and tasks DB migration

--- src/body.js
+++ src/body.js
@@ -1,47 +1,131 @@
 import React, { useEffect }  from "react";
-import { Box, Button, Card, CardBody, Center, Container, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, HStack, Input, Radio, RadioGroup, Spacer, Text, useDisclosure, VStack, Wrap } from "@chakra-ui/react";
-import {AddIcon, DeleteIcon} from '@chakra-ui/icons'
+import { Badge, Box, Button, ButtonGroup, Card, CardBody, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, HStack, Input, Radio, RadioGroup, Spacer, Spinner, Text, useDisclosure, useToast, VStack, Wrap } from "@chakra-ui/react";
+import {AddIcon, DeleteIcon, EditIcon} from '@chakra-ui/icons'
 import axios from "axios";
 
 function Body() {
+    const toast = useToast()
+
     // Drawer
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef()
-    // var arrayDataItems = []
 
-    // Radio
-    const [value, setValue] = React.useState('1')
-    //Tasks
+    //Tasks from get request are stored here
     const [tasks, setTasks] = React.useState(null)
 
-    useEffect (()=>{
-      axios.get("http://localhost:5000/users",  { crossdomain: true }).then(response => {
+    //Used to store task id for put requests
+    const [taskId, setTaskId] = React.useState(null)
+
+    //This is to differentiate between PUT and POST requests for the drawer
+    const [exists, setExists] = React.useState(false)
+
+    //This is used to post tasks
+    const [task, setTask] = React.useState(null);
+    const [platform, setPlatform] = React.useState(null);
+
+    const getRequestHandler = async () => {
+      await axios.get("http://localhost:5000/tasks",  { crossdomain: true }).then(response => {
         setTasks(response.data)
-        // console.log(response.data)
       });
-    })
+    };
+
+    const deleteRequestHandler = async (id) => {
+      await axios.delete(`http://localhost:5000/tasks/${id}`).then(response => {
+        if(response.status === 200) {
+          toast({
+            title: 'Task deleted.',
+            description: `Your task was successfully deleted`,
+            position:'top',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      });
+    };
+
+    const postRequestHandler = async () => {
+      const data = { "task": task, "platform":platform };
+     
+      await axios.post("http://localhost:5000/tasks", data).then(response=>{
+        if (response.status === 201) {
+          toast({
+            title: 'Task created.',
+            description: `Your task was created in ${platform}`,
+            status: 'success',
+            position:'top',
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      })
+      
+    };
+
+    const putRequestHandler = async (id) => {
+      const data = { "task": task, "platform":platform };
+     
+      await axios.put(`http://localhost:5000/tasks/${id}`, data).then(response => {
+        console.log(response)
+        if(response.status === 200) {
+          toast({
+            title: 'Task updated.',
+            description: `Your task was successfully updated`,
+            position:'top',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      });
+    };
+
+    //Loads all the data initially and checks for changes
+    useEffect (()=>{
+      getRequestHandler();
+    },[tasks])
 
     
     if (tasks === null) {
-      return <Text>Loading</Text>
+      return <Spinner size='xl' />
     }
     else{
       const arrayDataItems = tasks.map((task) =>
       
-        <Card w= '100%' boxShadow={'dark-lg'} variant={'elevated'} onClick={onOpen}>
+        <Card w= '100%' boxShadow={'dark-lg'} variant={'elevated'}>
                       <CardBody>
-        {/* <li> */}
-          {task.name.toString()}
-          {/* </li> */}
+                        <HStack>
+                        <VStack alignItems={'start'}>
+                          <Text>{task.task}</Text>
+          
+          <Badge>{task.platform}</Badge>
+          </VStack>
+          <Spacer/>
+          <ButtonGroup variant={'solid'} spacing='6' float={'right'}>
+  <Button colorScheme='blue' leftIcon={<EditIcon />} onClick={()=>{
+    setExists(true)
+    onOpen()
+    setTaskId(task.id)
+    setTask(task.task)
+    setPlatform(task.platform)
+    }}>Edit</Button>
+  <Button colorScheme='red' id={task.id} leftIcon={<DeleteIcon/>}
+  onClick={()=>{deleteRequestHandler(task.id)}}
+  >Delete</Button>
+</ButtonGroup>
+</HStack>
         </CardBody>
         </Card>
       
       );
     return (
       <>
-          <Button mt={'10'} alignSelf={'flex-end'} leftIcon={<AddIcon/>} colorScheme='green' onClick={
-            onOpen
-            }>New task</Button>
+          <Button mt={'10'} alignSelf={'flex-end'} leftIcon={<AddIcon/>} colorScheme='green' onClick={()=>{
+            setExists(false)
+            setTask(null)
+            setPlatform(null)
+            onOpen()
+            }}>New task</Button>
   
           
           <Box pt={'5'} alignItems={'flex-start'} w= '100%' justifyContent='space-between'>
@@ -63,28 +147,36 @@ function Body() {
   
             <DrawerBody>
               <VStack alignItems={'flex-start'}>
-              <Input placeholder='Type here...' />
+              <Input type="text"
+              isRequired
+              value={task}
+              onChange={(e) => setTask(e.target.value)} 
+              placeholder='Type here...' />
               <Spacer/><Spacer/>
               <Text as={'b'} fontSize={"xl"} alignSelf={'initial'}>Save my task on</Text>
               <Spacer/><Spacer/>
-              <RadioGroup onChange={setValue} value={value}>
+              <RadioGroup isDisabled={
+                exists?
+                true
+                :false
+              } isRequired onChange={(e)=>{setPlatform(e)}} value={platform}>
                   <Wrap px={'25px'} pb={'20'} pt={'2'}>
   
                       <Card w= '100%' boxShadow={'dark-lg'} variant={'elevated'}>
                           <CardBody>
-                  <Radio value='1'>Cubyts</Radio>
+                  <Radio value='Cubyts'>Cubyts</Radio>
                   </CardBody>
                   </Card>
                   
                   <Card w= '100%' boxShadow={'dark-lg'} variant={'elevated'}>
                   <CardBody>
-                  <Radio value='2'>Jira</Radio>
+                  <Radio value='Jira'>Jira</Radio>
                   </CardBody>
                   </Card>
   
                   <Card w= '100%' boxShadow={'dark-lg'} variant={'elevated'}>
                   <CardBody>
-                  <Radio value='3'>Asana</Radio>
+                  <Radio value='Asana'>Asana</Radio>
                   </CardBody>
                   </Card>
                   </Wrap>
@@ -96,7 +188,11 @@ function Body() {
               <Button variant='outline' mr={3} onClick={onClose}>
                 Cancel
               </Button>
-              <Button colorScheme='blue'>Save</Button>
+              <Button onClick={()=>{
+                exists?
+                putRequestHandler(taskId):
+                postRequestHandler()
+                }} colorScheme='blue'>Save</Button>
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
